8653571f6da8a92e5d08f4b92bb57d47a08824a0
Bajena <jan.bajena@productboard.com>
2024-01-21 13:18:58 +0100

    Control volume with a potentiometer

--- attinymuses.ino
+++ attinymuses.ino
@@ -7,15 +7,30 @@
 const uint32_t SPI_CLOCK = 100000;
 static const SPISettings sMusesSpiSettings(SPI_CLOCK, MSBFIRST, SPI_MODE2);
 
+static const uint16_t internalUnitMute = 0b111111111;
+static const uint16_t internalUnit0dB = 0b000100000;
+
+static const uint16_t leftChannelConfig = 0b0000010;
+static const uint16_t rightChannelConfig = 0b0000110;
+
 // pin definitions
 const int
   DATA_PIN(1),   // data in
   CLOCK_PIN(2),  // shift register clock
-  LATCH_PIN(3); // storage register clock (slave select)
+  LATCH_PIN(3), // storage register clock (slave select)
+  POT_PIN(A2); // PB4
 
-void setup() {
-  pinMode(LATCH_PIN, OUTPUT);
-  SPI.begin();
+uint16_t joinValues(uint16_t volumeValue, uint16_t channelConfigValue) {
+  return (uint16_t)volumeValue << 7 | channelConfigValue;
+}
+
+uint16_t fromQuarterOfDecibels(uint16_t quartersOfDb) {
+  return internalUnit0dB - quartersOfDb;
+}
+
+uint16_t setVolume(uint16_t volume) {
+  writeSpi(joinValues(volume, leftChannelConfig));
+  writeSpi(joinValues(volume, rightChannelConfig));
 }
 
 void writeSpi(uint16_t data) {
@@ -26,24 +41,17 @@ void writeSpi(uint16_t data) {
   SPI.endTransaction();
 }
 
+void setup() {
+  pinMode(POT_PIN, INPUT);
+  pinMode(LATCH_PIN, OUTPUT);
+  SPI.begin();
+}
+
 void loop() {
-  // L channel volume
-  writeSpi(0b0001000000000010);
-  // R channel volume 0db
-  writeSpi(0b0001000000000110);
+  long potentiometerValue = analogRead(POT_PIN);
+  uint16_t volumeLevel = (uint16_t)map(potentiometerValue, 0, 1023, 447, 0);
 
-  delay(1000);
+  setVolume(fromQuarterOfDecibels(-volumeLevel));
 
-  // L channel volume
-  writeSpi(0b11111111100000010);
-  // R channel volume 0db
-  writeSpi(0b11111111100000110);
-
-  delay(1000);
-
-  // Gain
-  // writeSpi(0b1000000000001010);
-
-  // // Config
-  // writeSpi(0b0000000000001110);
+  delay(10);
 }
